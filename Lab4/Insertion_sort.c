b09d0bb279ddf7ba912104c2305e7cfda6ac6549
Prabin Acharya <acharyaprabin1025y@gmail.com>
2025-07-05 23:25:35 +0545

    Update Insertion_sort.c

--- Lab4/Insertion_sort.c
+++ Lab4/Insertion_sort.c
@@ -21,26 +21,4 @@ void display(int A[], int n)
 int main()
 {
     int i, n, A[MAX];
-
-    clock_t start, end;
-    double time_taken;
-
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++)
-    {
-        A[i] = rand() % 1000;
-    }
-    display(A,n);
-
-    start = clock();
-    selection_sort(A, n);
-    end = clock();
-
-    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-
-    display(A, n);
-    printf("Time taken is %lf seconds\n", time_taken);
-    return 0;
 }
